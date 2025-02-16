112957e50c04c508565714db3f4605d1a1ab51b1
Feijian Ma <ma_feijian@163.com>
2025-02-16 21:22:52 +0800

    uv mapping by scale ctrl.

--- src/geom/utils/soup/GeometryHelper.d.ts
+++ src/geom/utils/soup/GeometryHelper.d.ts
@@ -1,4 +1,5 @@
+import { SoupUV } from './GeometryProducer';
 import { Shell } from '../../lib/@types/freebrep';
 export declare class GeometryHelper {
-    static getGeometry(shell: Shell): import("three").BufferGeometry[];
+    static getGeometry(shell: Shell, materialUVScales?: SoupUV): import("three").BufferGeometry[];
 }
